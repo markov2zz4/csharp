3d5209df1c1864b7ef850ff0d1b8da7d725e287d
MarkoW <albertnov@internet.ru>
2022-03-22 21:15:58 +0700

    Added Delegates

---
+++ Lektsii_SKOPIN/Program.cs
@@ -0,0 +1,27 @@
+﻿//Контрвариантность обобщений.
+
+Circle circle = new Circle();
+
+IContainer<Shape> container = new Container<Shape>(circle);
+//IContainer<Circle> container = new<Circle> (circle);
+
+public abstract class Shape { }
+
+public class Circle : Shape { }
+
+public interface IContainer<T>
+{
+    T Figure { get; set; }
+}
+
+public class Container<T> : IContainer<T>
+{
+    public Container(Circle circle)
+    {
+        Circle = circle;
+    }
+
+    public T Figure { get; set; }
+    public Circle Circle { get; }
+}
+
