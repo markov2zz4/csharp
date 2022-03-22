ec2d990b50de944ffc8a9467d4375d9c56f1e786
MarkoW <albertnov@internet.ru>
2022-03-22 21:15:16 +0700

    Added Delegates

---
+++ 04_07_Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04_07_Generics
+{
+
+    class Program
+    {
+
+        delegate object Del(object parametr);
+
+        static object DelegateCreate(object parametr)
+        {
+            return parametr;
+        }
+
+        static void Main()
+        {
+            Del ptr;
+            ptr = DelegateCreate;
+            DelegateCreate(ptr);
+
+        }
+    }
+}
